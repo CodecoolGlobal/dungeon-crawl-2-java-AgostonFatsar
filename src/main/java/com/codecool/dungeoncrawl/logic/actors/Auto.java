23a578e13c244d79a579fb18302abd3097425e98
Bence Töröcskei <torocskeibence@gmail.com>
2022-09-07 10:51:36 +0200

    Add: Auto ++

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Auto.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Auto.java
@@ -1,24 +1,39 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
+import com.codecool.dungeoncrawl.logic.GameMap;
 
-public class Auto extends Actor{
-
+public class Auto extends Actor {
+    private Cell cell = super.getCell();
 
     public Auto(Cell cell) {
         super(cell);
+        startCell = cell;
     }
 
+    int startX = 3;
+    int startY = 1;
 
+    Cell startCell;
 
     @Override
     public String getTileName() {
         return "auto";
     }
 
+
     public void moveCar() {
-        super.move(1, 0);
+        Cell nextCell = cell.getNeighbor(1, 0);
+        if (nextCell.getTileName().equals("wall")) {
+            cell.setActor(null);
+            cell = startCell.getNeighbor(-1,0);
+        } else {
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
+
+
     }
-
-
 }
